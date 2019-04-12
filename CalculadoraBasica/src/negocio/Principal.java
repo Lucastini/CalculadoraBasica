69d96480207cfe4a8edfa951bff5ac662d79d3cf
Lucastini <47428112+Lucastini@users.noreply.github.com>
2019-04-11 23:54:34 -0300

    Add files via upload

---
+++ CalculadoraBasica/src/negocio/Principal.java
@@ -0,0 +1,17 @@
+
+package negocio;
+
+import presentacion.CalculadoraSinNumeros;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        CalculadoraSinNumeros calSinN=new CalculadoraSinNumeros();
+        calSinN.setVisible(true);
+        
+        
+        
+        
+    }
+    
+}
